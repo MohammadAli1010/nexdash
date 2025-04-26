1865d9abe17ecdbb048617d70990d70156d24442
MohammadAli1010 <mohammadali991001@gmail.com>
2025-04-26 15:15:42 +0530

    navbar stufff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import { Button } from "@/components/ui/button";
-import { AudioWaveform } from "lucide-react";
 
 const Homepage = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="xl" className="rounded-full">
-        <AudioWaveform />
-        Click Me!
-      </Button>
-    </div>
-  );
+  return <div className=""></div>;
 };
 
 export default Homepage;
